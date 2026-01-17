ce67a69f4775daba94463905ca9d222750065d12
raqibreyaz <raquibreyaz111@gmail.com>
2026-01-17 23:04:55 +0530

    learnt about path module

---
+++ 11-Building-RESTful-APIs-with-Express/19-understanding-path-module.js
@@ -0,0 +1,22 @@
+import path from "node:path";
+
+console.log(path.join("/", "../../../hello", "hi"));
+// /hello/hi
+
+console.log(path.normalize("./../../test"));
+// /test
+
+console.log(path.resolve("hi", "test"));
+// /mnt/data1/my-files/Backend-Mastery/hi/test
+
+console.log(path.resolve("/hi", "test"));
+// /hi/test
+
+console.log(path.basename('/mnt/data1/my-files/Backend-Mastery/hi/test'));
+// test
+
+console.log(path.dirname('/mnt/data1/my-files/Backend-Mastery/hi/test'));
+// /mnt/data1/my-files/Backend-Mastery/hi
+
+console.log(import.meta.dirname)
+// /mnt/data1/my-files/Backend-Mastery/11-Building-RESTful-APIs-with-Express
