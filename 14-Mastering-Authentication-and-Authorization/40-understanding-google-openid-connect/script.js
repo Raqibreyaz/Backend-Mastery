a2c9b8d09dea1c6eec929cef55b6d6fa2ae30882
raqibreyaz <raquibreyaz111@gmail.com>
2026-02-11 22:32:01 +0530

    learnt about using google's openid connect to get user's details

---
+++ 14-Mastering-Authentication-and-Authorization/40-understanding-google-openid-connect/script.js
@@ -0,0 +1,24 @@
+const code = new URLSearchParams(location.search).get("code");
+const client_id = `<client_id>`;
+const client_secret = `<client_secret>`;
+const redirect_uri = `http://localhost:3000`;
+
+async function fetchIdToken() {
+  const body = `code=${code}&client_id=${client_id}&client_secret=${client_secret}&redirect_uri=${redirect_uri}&grant_type=authorization_code`;
+
+  const res = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body,
+  });
+  const data = await res.json();
+  console.log(data);
+  const userToken = data.id_token.split(".")[1];
+  console.log(userToken);
+  const userData = JSON.parse(atob(userToken));
+  console.log(userData);
+}
+
+if (code) fetchIdToken();
