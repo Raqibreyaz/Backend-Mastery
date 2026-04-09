bed0149e4c4a4b8dbe068f95fb6301dbe6e4b969
Raquib Reyaz <raquibreyaz111@gmail.com>
2026-04-09 13:57:47 +0530

    completed s3 section, lastly learnt about uploading using signed url with cors enabled

--- 18-Cloud-Storage-Integration-With-AWS-S3/31-CORS-Configuration-in-S3-Bucket/app.js
+++ 18-Cloud-Storage-Integration-With-AWS-S3/31-CORS-Configuration-in-S3-Bucket/app.js
@@ -5,13 +5,14 @@ const s3Client = new S3Client();
 
 const command = new PutObjectCommand({
   Bucket: "bucket_name",
-  Key: "hello-world-2.png",
-  ContentType: "image/png",
+  Key: "image-1.png",
+  ContentLength: 106764,
 });
 
 const signedUrl = await getSignedUrl(s3Client, command, {
   expiresIn: 5 * 60,
-  signableHeaders: new Set(["content-type"]),
+  signableHeaders: new Set(["content-length"]),
 });
 
 console.log(signedUrl);
+  
